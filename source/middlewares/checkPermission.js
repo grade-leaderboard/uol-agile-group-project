ed3426925fe11eaea22994c1b191cf5ecdaf6055
Blair Currey <curreyb88@gmail.com>
2021-01-23 10:45:42 -0500

    edit grades

--- source/middlewares/checkPermission.js
+++ source/middlewares/checkPermission.js
@@ -1,30 +1,29 @@
 // middleware for blocking access to routes based on permissions
 async function checkPermission(req, res, next) {
-
 	const action = `${req.method} ${req.path.toLowerCase()}`;
 	let permit = false;
 
 	try {
 		switch (action) {
-			case 'POST /editgrades': {
-				[user, _] = await db.query('SELECT user_id FROM grades WHERE id=?', [req.body.id]);
-				permit = req.user.id == user;
+			case "POST /edit_grades": {
+				[user, _] = await db.query("SELECT user_id FROM grades WHERE id=?", [req.body.grade_id]);
+				permit = req.user.id == user[0].user_id;
+				break;
 			}
-			case 'POST /addgrade': {
-				permit = true
-			}			
+			case "POST /addgrade": {
+				permit = true;
+				break;
+			}
 		}
 		if (permit) {
 			next();
 		} else {
 			res.status(403).send("<h3>403: Forbidden</h3>");
 		}
-
 	} catch (error) {
 		console.log(error);
 		res.status(500).send("<h3>500: Internal server error</h3>");
 	}
-
 }
 
 module.exports = checkPermission;
