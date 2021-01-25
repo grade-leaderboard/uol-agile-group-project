9c3699d33bf0a72c9cee1071c4a53f5598bddac3
Blair Currey <12960453+BlairCurrey@users.noreply.github.com>
2021-01-25 11:52:19 -0500

    GL-21 Edit submitted grades

--- source/routes/main.js
+++ source/routes/main.js
@@ -1,18 +1,19 @@
 const checkAuth = require("../middlewares/checkAuth");
 const checkPermission = require("../middlewares/checkPermission");
-const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require("express-validator");
 
 module.exports = function (app) {
 	app.get("/", async (req, res) => {
 		try {
 			console.log(req.user.name);
-			let sql = "SELECT  m.id, m.title, m.grade, COUNT(g.course_id) AS 'submissions' \
+			let sql =
+				"SELECT  m.id, m.title, m.grade, COUNT(g.course_id) AS 'submissions' \
 								FROM modules_with_grades m \
 								LEFT JOIN grades g \
 								ON g.course_id = m.id\
 								GROUP BY m.id\
-								ORDER BY id ASC" ; 
-								
+								ORDER BY id ASC";
+
 			var [results, _] = await db.query(sql);
 			res.render("index.html", {
 				res: results,
@@ -27,11 +28,11 @@ module.exports = function (app) {
 			let sql = "SELECT * FROM courses ORDER BY id ASC";
 			var [courses, _] = await db.query(sql);
 			sql = "SELECT * FROM study_sessions ORDER BY id ASC";
-			var [semesters, _] = await db.query(sql);			
+			var [semesters, _] = await db.query(sql);
 			res.render("addgrade.html", {
-				title: 'Leaderboard - Add grade',
+				title: "Leaderboard - Add grade",
 				heading: "Add grade",
-				courseList: courses, 
+				courseList: courses,
 				semesterList: semesters,
 				addResult: req.query.addResult,
 			});
@@ -42,8 +43,7 @@ module.exports = function (app) {
 
 	app.post("/addgrade", checkAuth, checkPermission, async (req, res) => {
 		try {
-			let params = [req.body.course_id, req.body.semester, req.user.id, 
-				req.body.grade, !!req.body.anonymous]
+			let params = [req.body.course_id, req.body.semester, req.user.id, req.body.grade, !!req.body.anonymous];
 			let sql = `
 			INSERT INTO grades(course_id, study_session_id, user_id, grade, anonymous)
 			VALUES (?, ?, ?, ?, ?)`;
@@ -51,10 +51,9 @@ module.exports = function (app) {
 			res.redirect(req.baseUrl + "?addResult=success");
 		} catch (error) {
 			console.log(error);
-            if (error.code == "ER_DUP_ENTRY") {
-				res.redirect(req.baseUrl + `?addResult=You already have a grade for module ${req.body.course_id}. You may edit existing grade`
-				)
-			}	
+			if (error.code == "ER_DUP_ENTRY") {
+				res.redirect(req.baseUrl + `?addResult=You already have a grade for module ${req.body.course_id}. You may edit existing grade`);
+			}
 		}
 	});
 
@@ -82,11 +81,11 @@ module.exports = function (app) {
 	});
 
 	app.get("/personal_grade", checkAuth, async (req, res) => {
-		try{
-			let user = [req.user.id]
+		try {
+			let user = [req.user.id];
 			// console.log(req.user.name);
-			let sql = 
-			"SELECT study_sessions.title AS session, grades.course_id, courses.title, grades.grade\
+			let sql =
+				"SELECT study_sessions.title AS session, grades.course_id, courses.title, grades.grade\
 					FROM grades \
 					JOIN users \
 					ON grades.user_id = users.id \
@@ -96,11 +95,54 @@ module.exports = function (app) {
 					ON courses.id = grades.course_id \
 					WHERE users.id = ? ";
 			var [results, _] = await db.query(sql, user);
-			res.render("personal_grade.html", {user: req.user.name, res: results});
-
-		} catch(error){
+			res.render("personal_grade.html", { user: req.user.name, res: results });
+		} catch (error) {
 			console.log(error);
 		}
+	});
 
+	app.get("/edit_grades", checkAuth, async (req, res) => {
+		try {
+			let grades_sql =
+				"SELECT courses.id AS course_id, \
+					courses.title AS course_title, \
+					grades.id AS grade_id \
+					FROM grades \
+					JOIN users \
+					ON grades.user_id = users.id \
+					JOIN courses \
+					ON courses.id = grades.course_id \
+					WHERE users.name = ?";
+			let sessions_sql = "SELECT id, title FROM study_sessions";
+			var [grades_results, _] = await db.query(grades_sql, [req.user.name]);
+			var [sessions_results, _] = await db.query(sessions_sql);
+
+			res.render("edit_grades.html", {
+				title: "Leaderboard - Edit grades",
+				heading: "Edit grades",
+				grades_res: grades_results,
+				sessions_res: sessions_results,
+				editResult: req.query.editResult,
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	});
+
+	app.post("/edit_grades", checkAuth, checkPermission, async (req, res) => {
+		try {
+			let sql = "UPDATE grades \
+					SET study_session_id = ?, \
+					grade = ?, \
+					anonymous = ? \
+					WHERE id = ?";
+			var anonymous = req.body.anonymous == "on" ? 1 : 0;
+			var fields = [req.body.session, req.body.grade, anonymous, req.body.grade_id];
+			var [results, _] = await db.query(sql, fields);
+			res.redirect(req.baseUrl + "?editResult=success");
+		} catch (error) {
+			console.log(error);
+			res.redirect(req.baseUrl + "?editResult=Grade was not edited. Something went wrong.");
+		}
 	});
 };
