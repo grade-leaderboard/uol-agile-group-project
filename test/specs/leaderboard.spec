f9ca0ff97ff39d4d50720dcb2a0d0062140858e1
Brad Lazaruk <brad@lazaruk.com>
2021-01-25 14:08:46 -0700

    functioning gauge and taiko
    Although, you'll need to install gauge and taiko locally to get the testing to work

---
+++ test/specs/leaderboard.spec
@@ -0,0 +1,32 @@
+# Test Public Pages
+Remember to reset the database prior to the tests
+
+## View default page 
+* Open default app webpage
+
+## View personal grades page
+* Open personal grades page
+
+## Add a valid grade
+* Open addgrade page
+* Select second module
+* Select first session
+* Add a invalid negative grade
+* Add a invalid grade above 100
+* Add a valid grade
+* Check for grade entered success message
+
+## Add a valid grade anonymously
+* Open addgrade page
+* Select fourth module
+* Select third session
+* Click submit anonymously button
+* Add a valid grade
+* Check for grade entered success message
+
+## Add a valid grade where one was already entered
+* Open addgrade page
+* Select second module
+* Select first session
+* Add a valid grade
+* Check for you already have a grade message
--- test/tests/leaderboard_step_implementations.js
+++ test/tests/leaderboard_step_implementations.js
@@ -43,17 +43,14 @@ step("Change device to iPhone X", async () => {
 
 step("Open default app webpage", async () => {
     await goto("127.0.0.1:8080");
-    await screenshot({path: 'reports\\screenshots\\open_default_page.png'})
 });
 
 step("Open personal grades page", async () => {
     await goto("127.0.0.1:8080/personal_grade");
-    await screenshot({path: 'reports\\screenshots\\open_personal_grades_page.png'})
 });
 
 step("Open addgrade page", async () => {
     await goto("127.0.0.1:8080/addgrade");
-    await screenshot({path: 'reports\\screenshots\\validate_addgrade_page.png'})
 });
 
 step("Select second module", async () => {
@@ -80,33 +77,32 @@ step("Add a valid grade", async () => {
     await clear(textBox({id:'grade'}))
     await write('72',into(textBox({id:'grade'})))
     await click('Submit');
-    await screenshot({path: 'reports\\screenshots\\validate_valid_grade_entered.png'})
 });
 
 step("Add a invalid negative grade", async () => {
     await clear(textBox({id:'grade'}))
     await write('-7',into(textBox({id:'grade'})))
     await click('Submit');
-    await screenshot({path: 'reports\\screenshots\\validate_negative_grade_entered.png'})
 });
 
 step("Add a invalid grade above 100", async () => {
     await clear(textBox({id:'grade'}))
     await write('107',into(textBox({id:'grade'})))
     await click('Submit');
-    await screenshot({path: 'reports\\screenshots\\validate_grade_over_100_entered.png'})
 });
 
 step("Check for grade entered success message", async () => {
     await assert.ok(await text('Your grade was successfully added', {exactMatch: false}).exists());
-    await screenshot({path: 'reports\\screenshots\\validate_grade_successfully_entered.png'})
 });
 
 step("Check for you already have a grade message", async () => {
     await assert.ok(await text('You already have a grade for module', {exactMatch: false}).exists());
-    await screenshot({path: 'reports\\screenshots\\validate_grade_already_exists.png'})
 });
 
 step("Click submit anonymously button", async () => {
     await click('Do not reveal my identity in leaderboard. Keep this grade anonymous.')
+});
+
+step("Take a screenshot", async () => {
+    await screenshot({path: 'reports\\screenshots\\validate_grade_already_exists.png'})
 });
