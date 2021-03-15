1c06f8d4cdd5f3e2cb6d05dffdccc7703a65fb62
Blair Currey <curreyb88@gmail.com>
2021-03-15 10:26:13 -0400

    updated deprecated body-parser initialization

--- source/index.js
+++ source/index.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
 const db = require("mysql-promise")();
 const path = require("path");
 const passport = require("passport");
@@ -10,17 +9,17 @@ const putObjectsInAllViews = require("./middlewares/putObjectsInAllViews");
 const flash = require("flash");
 
 db.configure({
-	connectionLimit : 1000,
-    connectTimeout  : 60 * 60 * 1000,
-    acquireTimeout  : 60 * 60 * 1000,
-    timeout         : 60 * 60 * 1000, 
+	connectionLimit: 1000,
+	connectTimeout: 60 * 60 * 1000,
+	acquireTimeout: 60 * 60 * 1000,
+	timeout: 60 * 60 * 1000,
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_DATABASE,
 	ssl: {
-		rejectUnauthorized: false 
-	}
+		rejectUnauthorized: false,
+	},
 });
 
 global.db = db;
@@ -29,7 +28,8 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, "/public")));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const sessionStore = new MySQLStore({}, db);
 app.use(
@@ -38,7 +38,7 @@ app.use(
 		secret: process.env.SESSION_SECRET,
 		store: sessionStore,
 		resave: false,
-		saveUninitialized: false, 
+		saveUninitialized: false,
 	})
 );
 app.use(passport.initialize());
@@ -48,24 +48,24 @@ app.use(flash());
 require("./routes/main")(app, passport);
 require("./config/passport")(passport);
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
 	res.status(404);
-  
+
 	// respond with html page
-	if (req.accepts('html')) {
-	  res.render('pages/error.html', { url: escape(req.url) });
-	  return;
+	if (req.accepts("html")) {
+		res.render("pages/error.html", { url: escape(req.url) });
+		return;
 	}
-  
+
 	// respond with json
-	if (req.accepts('json')) {
-	  res.send({ error: 'Not found' });
-	  return;
+	if (req.accepts("json")) {
+		res.send({ error: "Not found" });
+		return;
 	}
-  
+
 	// default to plain-text. send()
-	res.type('txt').send('Not found');
-  });
+	res.type("txt").send("Not found");
+});
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
